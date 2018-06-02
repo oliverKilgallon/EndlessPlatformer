c0ae077cda95fb6f63087cf60bc396e7b4fc1688
oliver kilgallon <u1654523@unimail.hud.ac.uk>
2018-06-02 17:16:23 +0100

    Adding builds to repo

--- Assets/Scripts/Interactables/Killzone.cs
+++ Assets/Scripts/Interactables/Killzone.cs
@@ -4,10 +4,16 @@ public class Killzone : MonoBehaviour {
     
     public GameObject leftEnd;
     public GameObject rightEnd;
+<<<<<<< HEAD
     public float baseSpeed = 0.5f;
     public float divisor = 5f;
     private float ascentSpeed;
+=======
+    public float baseSpeed;
+    public float divisor;
+>>>>>>> Adding builds to git repo
 
+    private float ascentSpeed;
     private LineRenderer line;
 
     private PlayerStats playerStats;
@@ -25,6 +31,7 @@ public class Killzone : MonoBehaviour {
 
     private void Update ()
     {
+<<<<<<< HEAD
         if (!playerStats.isPaused)
         {
             //Ascent speed is dependant on distance from player + a base speed
@@ -38,8 +45,22 @@ public class Killzone : MonoBehaviour {
         }
 
         //Reset line positions
-        line.SetPosition(0, leftEnd.transform.position);
+=======
+        //Speed is greater the further the killzone is from the player
+        ascentSpeed = baseSpeed + 
+            ( -( transform.position.y - GameManager.instance.player.transform.position.y ) / 5f);
+        
+        //Adjust position
+        transform.position += new Vector3(0, 1, 0) * ascentSpeed * Time.deltaTime;
 
+        transform.position = new Vector3(
+            GameManager.instance.player.transform.position.x, 
+            transform.position.y, 
+            transform.position.z);
+
+        //Reset line renderer positions
+>>>>>>> Adding builds to git repo
+        line.SetPosition(0, leftEnd.transform.position);
         line.SetPosition(1, rightEnd.transform.position);
     }
 
